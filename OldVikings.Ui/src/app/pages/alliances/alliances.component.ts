ccd46a252f259e98c24e6b0348bc06fe556d7c35
Tomasi - Developing <info@tomasi-developing.ch>
2024-07-31 15:01:14 +0200

    fix

---
+++ OldVikings.Ui/src/app/pages/alliances/alliances.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alliances',
+  templateUrl: './alliances.component.html',
+  styleUrl: './alliances.component.scss'
+})
+export class AlliancesComponent {
+
+}
