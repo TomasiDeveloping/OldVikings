a72803c24c4eb83369a071a94d8860e9bac60a0e
Tomasi - Developing <info@tomasi-developing.ch>
2024-08-21 14:13:16 +0200

    fix nap

--- OldVikings.Ui/src/app/navigation/navigation.component.ts
+++ OldVikings.Ui/src/app/navigation/navigation.component.ts
@@ -8,7 +8,7 @@ import { ToastrService} from "ngx-toastr";
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent implements OnInit{
-  version: string = '1.7.2';
+  version: string = '1.7.3';
   isShown: boolean = false;
   siteLanguage: string = 'Deutsch';
   languageList = [
