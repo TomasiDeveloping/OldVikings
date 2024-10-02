e655bb2d433eb95d76811dec1e6fdc0d44f7449f
Tomasi - Developing <info@tomasi-developing.ch>
2024-10-02 07:05:51 +0200

    change allie and member password

--- OldVikings.Ui/src/app/navigation/navigation.component.ts
+++ OldVikings.Ui/src/app/navigation/navigation.component.ts
@@ -8,7 +8,7 @@ import { ToastrService} from "ngx-toastr";
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent implements OnInit{
-  version: string = '1.13.0';
+  version: string = '1.14.0';
   isShown: boolean = false;
   siteLanguage: string = 'Deutsch';
   languageList = [
--- OldVikings.Ui/src/app/services/member.service.ts
+++ OldVikings.Ui/src/app/services/member.service.ts
@@ -6,7 +6,7 @@ import {Injectable} from '@angular/core';
 export class MemberService {
 
   private readonly _localStorageKey = 'oldVikingsKey';
-  private readonly _currentKey = 'Welcome2024';
+  private readonly _currentKey = 'Vikings2024';
 
   private redirectUri: string | null = null;
 
