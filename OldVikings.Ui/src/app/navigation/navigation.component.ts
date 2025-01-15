e0ae67dc5faa9b426a774e1835f3b966f78919f1
Tomasi - Developing <info@tomasi-developing.ch>
2025-01-15 09:02:16 +0100

    add secret tasks

--- OldVikings.Ui/src/app/navigation/navigation.component.ts
+++ OldVikings.Ui/src/app/navigation/navigation.component.ts
@@ -8,7 +8,7 @@ import { ToastrService} from "ngx-toastr";
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent implements OnInit{
-  version: string = '1.22.2';
+  version: string = '1.23.0';
   isShown: boolean = false;
   siteLanguage: string = 'Deutsch';
   languageList = [
