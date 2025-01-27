86b36bac2583c99992d8b1eac1510f42419aef1e
Tomasi - Developing <info@tomasi-developing.ch>
2025-01-27 11:05:35 +0100

    remove GitHub action

--- OldVikings.Ui/src/app/services/member.service.ts
+++ OldVikings.Ui/src/app/services/member.service.ts
@@ -6,7 +6,7 @@ import {Injectable} from '@angular/core';
 export class MemberService {
 
   private readonly _localStorageKey = 'oldVikingsKey';
-  private readonly _currentKey = 'Valhalla@24';
+  private readonly _currentKey = 'Odin@2025';
 
   private redirectUri: string | null = null;
 
