0cd43eb6591b81d335c836bf50f631b9978de049
Tomasi - Developing <info@tomasi-developing.ch>
2025-06-24 12:40:22 +0200

    R4Roles

--- OldVikings.Ui/src/app/navigation/navigation.component.ts
+++ OldVikings.Ui/src/app/navigation/navigation.component.ts
@@ -8,7 +8,7 @@ import { ToastrService} from "ngx-toastr";
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent implements OnInit{
-  version: string = '1.29.78';
+  version: string = '1.30.00';
   isShown: boolean = false;
   siteLanguage: string = 'Deutsch';
   languageList = [
